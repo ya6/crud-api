4c5a71639c070e59370f19d7d7942dacc6594e6a
alex <emal@damain.com>
2023-07-01 18:40:18 +0200

    feat: add post feature

--- src/server.ts
+++ src/server.ts
@@ -4,19 +4,24 @@ import { UserService } from "./service";
 
 //
 const userService = new UserService();
-console.log(http.STATUS_CODES[404]);
 const PORT = 4000;
 const requestListener = (req, res) => {
   const { method, url } = req;
   const URL = parse(url, true);
-  // console.log(URL);
+  let segmented_pathname: string[] = [];
+  if (URL.pathname) {
+    segmented_pathname = URL.pathname.split("/");
+  }
 
+  // console.log(URL);
+  // console.log(http.STATUS_CODES[404]);
+  // get /
   if (method === "GET" && URL.pathname === "/") {
     res.statusCode = 200;
     res.end(`${"Home"}`);
   }
+  // get/api/users
   if (method === "GET" && URL.pathname === "/api/users") {
-    // res.end(`${method} --- ${URL.pathname}`);
     const users = userService.getAllUsers();
     const strUsers = JSON.stringify(users);
     res.statusCode = 200;
@@ -27,23 +32,50 @@ const requestListener = (req, res) => {
       return res.end(`error: ${error.message}`);
     }
   }
-  let body: any[] = [];
-  req
-    .on("data", (chank) => {
-      body.push(chank);
-    })
-    .on("end", () => {
-      try {
-        const bufferToString = Buffer.concat(body).toString("utf-8");
-        const parsedJson = JSON.parse(bufferToString);
-        console.log(parsedJson);
-        res.statusCode = 201;
-        return res.end();
-      } catch (error: any) {
-        res.statusCode = 400;
-        return res.end(`error: ${error.message}`);
-      }
-    });
+  // get/api/users/id
+  if (method === "GET" && segmented_pathname[1] === "api" && segmented_pathname[2] === "users" && !!segmented_pathname[3]) {
+    const checkedId = userService.validateId(segmented_pathname[3]);
+    if (checkedId === "invalid") {
+      res.statusCode = 400;
+      return res.end(`invalid ID`);
+    }
+    const user = userService.getUserById(checkedId);
+    console.log(checkedId);
+    console.log(user);
+    if (user) {
+      res.statusCode = 200;
+      return res.end(JSON.stringify(user));
+    }
+    res.statusCode = 404;
+    return res.end(`user doesn't exist`);
+  }
+
+  if (method === "POST" && URL.pathname === "/api/users") {
+    let body: any[] = [];
+    req
+      .on("data", (chank) => {
+        body.push(chank);
+      })
+      .on("end", () => {
+        try {
+          const userDto = userService.parseToJson(body);
+          const validatedErrors = userService.validateUserDto(userDto);
+
+          if (validatedErrors.length > 0) {
+            res.statusCode = 400;
+            return res.end(JSON.stringify(validatedErrors));
+          }
+          
+          // create user
+          const user = userService.createUser(userDto);
+          res.statusCode = 201;
+          return res.end(JSON.stringify(user));
+        } catch (error: any) {
+          res.statusCode = 400;
+          return res.end(`error: ${error.message}`);
+        }
+      });
+  }
 };
 
 const server = http.createServer();
--- src/service.ts
+++ src/service.ts
@@ -1,13 +1,50 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4, validate } from "uuid";
+// import * as uuid from "uuid";
 
 type User = { id: string; usename: string; age: number; hobbies: string[] };
 
 export class UserService {
-  users: User[] = [
+  public users: User[] = [
     { id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d", usename: "Alex", age: 48, hobbies: ["dev", "travel"] },
     { id: "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed", usename: "Yan", age: 1, hobbies: [] },
   ];
   getAllUsers() {
     return this.users;
+  }
+
+  getUserById(id: string) {
+    const user = this.users.find((el) => el.id === id);
+    return user;
+  }
+
+  createUser(userDto) {
+    const newUser = { id: uuidv4(), ...userDto };
+    this.users.push(newUser);
+    return newUser;
+  }
+  validateUserDto(userdto) {
+    const { username, age, hobbies } = userdto;
+
+    const validateErrors: string[] = [];
+    if (!username || username.length === 0 || typeof username !== "string") {
+      validateErrors.push("username in requered as not empty string");
+    }
+    if (!age || age <= 0 || typeof age !== "number") {
+      validateErrors.push("age in requered as positive number");
+    }
+    if (!hobbies || !Array.isArray(hobbies)) {
+      validateErrors.push("hobbies in requered as array");
+    }
+    return validateErrors;
+  }
+  // helpers
+  validateId(id: string) {
+    const checkedId = validate(id) ? id : "invalid";
+    return checkedId;
+  }
+  parseToJson(body: any[]) {
+    const bufferToString = Buffer.concat(body).toString("utf-8");
+    const parsedJson = JSON.parse(bufferToString);
+    return parsedJson;
   }
 }
